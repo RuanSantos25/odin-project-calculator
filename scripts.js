b0a618027998e69771f8012e14e3a11483b7ed3b
RuanSantos25 <[email protected]>
2024-09-08 11:17:46 -0300

    Refactor the multiply function
    This will make the multiply function to display the result on the
    calculator screen instead of the console.

--- scripts.js
+++ scripts.js
@@ -18,7 +18,10 @@ function subtract(num1, num2) {
 }
 
 function multiply(num1, num2) {
-    console.log(num1 * num2);
+    const result = num1 * num2;
+    displayNumbers.textContent = result;
+    number1 = result;
+    operator = undefined;
 }
 
 function divide(num1, num2) {
